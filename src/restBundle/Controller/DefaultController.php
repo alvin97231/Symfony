8d37d8041a637bcf1ed6fdb253011e4ecb0ae494
cedlio <lionnet.cedric@gmail.com>
2016-05-17 13:44:33 +0200

    test route

--- src/restBundle/Controller/DefaultController.php
+++ src/restBundle/Controller/DefaultController.php
@@ -3,11 +3,19 @@
 namespace restBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($foo, $foofoo)
     {
-        return $this->render('restBundle:Default:index.html.twig');
+        $arr['foo'] = $foo;
+        $arr['foofoo'] = $foofoo;
+        $response = new Response();
+        $response->setContent(json_encode($arr));
+        $response->setStatusCode(Response::HTTP_OK);
+        $response->headers->set('Content-Type', 'text/html');
+        
+        return $response;
     }
 }
