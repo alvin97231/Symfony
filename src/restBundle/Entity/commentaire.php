d23fb49e593dd371ea00618a5fa9429954d58008
cedric <lionnet.cedricgmail.com>
2016-06-05 20:07:17 +0200

    conversion commentaire.contenu et idee.contenu de VARCHAR(255) vers LONGTEXT

--- src/restBundle/Entity/commentaire.php
+++ src/restBundle/Entity/commentaire.php
@@ -24,7 +24,7 @@ class commentaire
     /**
      * @var string
      *
-     * @ORM\Column(name="contenu", type="string", length=255)
+     * @ORM\Column(name="contenu", type="text")
      */
     private $contenu;
 
